577adb52821f48654846fd3c7e0606baf0ccf078
Matheus Tecchio <matheusdiastecchio@gmail.com>
2024-07-01 16:49:16 +0100

    reading os file and switch cases

--- main.go
+++ main.go
@@ -2,37 +2,46 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"strconv"
+	"log"
+	"os"
 )
 
-func greet(name ...string) {
-	var userName string
-	if len(name) > 0 {
-		userName = name[0]
-	} else {
-		userName = "User"
+func readFile(path string) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Panic(err)
 	}
-	fmt.Println("Hi", userName)
+
+	data := make([]byte, 100)
+	if _, err := file.Read(data); err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(string(data))
 }
 
-func convertAndSum(a int, b string) (int, error) {
-	i, err := strconv.Atoi(b)
-	if err != nil {
-		return 0, err
+var heads, tails int
+
+func flipCoin(side string) {
+
+	switch side {
+	case "head":
+		heads++
+	case "tail":
+		tails++
+	default:
+		fmt.Println("Coin is straight")
 	}
 
-	total := a + i
-
-	return total, nil
+	fmt.Print("\nHeads: ", heads, "\nTails: ", tails, "\n")
 }
 
 func main() {
-	fmt.Println(math.Pi)
-	name := "matheus"
-	fmt.Println(name)
+	readFile("hello.txt")
 
-	greet("Matheus")
+	flipCoin("head")
+	flipCoin("head")
+	flipCoin("tail")
 
-	fmt.Println(convertAndSum(10, "5"))
 }
