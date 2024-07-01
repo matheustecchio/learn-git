e13685bf2e73b9525c147d4723eb80741a5f46f9
Matheus Tecchio <matheusdiastecchio@gmail.com>
2024-07-01 21:13:09 +0100

    maps

--- main.go
+++ main.go
@@ -3,31 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	// Slices: Can increase and decrease in capacity. As the capacity reach its top limit, Go double its capacity in memory.
-	// The first capacity of a slice declared with only `:=` is the length of it.
-	names := []string{"Tiago", "Daniel", "Marcos", "Marta"}
-	fmt.Println(names, len(names), cap(names))
+	ages := make(map[string]int8)
+	ages["Tiago"] = 31
+	ages["Marta"] = 43
+	ages["Daniel"] = 26
+	ages["Matheus"] = 23
 
-	names = append(names, "Rafael")
-	fmt.Println(names, len(names), cap(names))
+	fmt.Println(ages)
+	fmt.Println(ages["Tiago"])
+	fmt.Println(ages["Lucas"]) // As `Lucas` doesn't exist, it returns the zero-value of the type, in this case, 0.
 
-	names = append(names, "Roberta")
-	fmt.Println(names, len(names), cap(names))
-
-	names = append(names, "Rafael")
-	fmt.Println(names, len(names), cap(names))
-
-	names = append(names, "Matheus")
-	fmt.Println(names, len(names), cap(names))
-
-	names = append(names, "John")
-	fmt.Println(names, len(names), cap(names))
-
-	// If we want to make slice with a capacity bigger than the length of it, we can use the make() function.
-	students := make([]string, 0, 15) // It starts the slice with 0 values but with 15 of capacity in memory.
-	fmt.Println(students, len(students), cap(students))
-
-	pizza := make([]string, 8) // It starts the slice with 8 values and a capacity of 8 as well.
-	fmt.Println(pizza, len(pizza), cap(pizza))
+	// We can assign two variables to a map value, the first return the value itself and the second returns a boolean, true if the key exists and false if not.
+	value, ok := ages["Marta"]
+	fmt.Println(value, ok)
 
 }
