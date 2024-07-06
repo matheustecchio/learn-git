33189203d2e3eddf6c278c074619118216eba432
Matheus Tecchio <matheusdiastecchio@gmail.com>
2024-07-06 19:45:27 +0100

    add .txt database and its logic to save it

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 var balance float64
@@ -68,9 +69,43 @@ func withdrawMoney() {
 
 }
 
+// If a file doesn't exist, it creates the file with the designated name and format, and insert the value 0.
+func createFile(fileName string) {
+	_, err := os.ReadFile(fileName)
+	if err != nil {
+		os.Create(fileName)
+		os.WriteFile(fileName, []byte("0"), 0644)
+	}
+}
+
+// Synchronizes the file data to a variable in our code.
+func syncData(fileName string) float64 {
+	fileByte, err := os.ReadFile(fileName)
+	if err != nil {
+		log.Fatalf("File '%s' doesn't exist or cannot be read: %v", fileName, err)
+	}
+
+	byteToString := string(fileByte)
+	stringToFloat64, err := strconv.ParseFloat(byteToString, 64)
+	if err != nil {
+		log.Fatalf("Failed to convert balance data from file '%s' to float64: %v", fileName, err)
+	}
+
+	return stringToFloat64
+}
+
+func saveDataToFile(fileName string, targetFloat float64) {
+	floatToString := fmt.Sprint(targetFloat)
+	os.WriteFile(fileName, []byte(floatToString), 0644)
+}
+
 func main() {
+	createFile("balance.txt")
+	balance = syncData("balance.txt")
+
 	for {
 		displayMenu()
+		saveDataToFile("balance.txt", balance)
 	}
 
 }
